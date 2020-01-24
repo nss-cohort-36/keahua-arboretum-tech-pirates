84eabb4eb6da1b64580c28c506a7e966d21fae0d
Christian Pippin <ianpippin@gmail.com>
2020-01-24 09:46:14 -0600

    more comments

--- actions/annex.py
+++ actions/annex.py
@@ -2,14 +2,16 @@ import os
 from environments import River
 
 # cp Need to add proper imports
-def annex_habitat(arboretum):
+def annex_biome(arboretum):
     os.system('cls' if os.name == 'nt' else 'clear')
     print("1. River")
     print("2. Swamp")
     print("3. Coastline")
     print("4. Grassland")
+    print("5. Mountain")
+    print("6. Forest")
 # cp Need to add mountain and forest to menu
-    choice = input("Choose your habitat > ")
+    choice = input("Choose what you want to annex? > ")
 # cp Need to change strings to display appropriately based on the README
     if choice == "1":
         river = River()
--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import os
 from arboretum import Arboretum
-from actions.annex import annex_habitat
+from actions.annex import annex_biome
 from actions.release_animal import release_animal
 from actions.report import build_facility_report
 # ADDED THIS IMPORT TO PRODUCE MENU WHEN NUMBER 4 IS CHOSEN. NEEDS TO BE UNCOMMENTED FOR CODE ON CHOICE 4 TO WORK:
@@ -10,7 +10,7 @@ keahua = Arboretum("Keahua Arboretum", "123 Paukauila Lane")
 
 def build_menu():
     os.system('cls' if os.name == 'nt' else 'clear')
-    print("1. Annex Habitat")
+    print("1. Annex Biome")
     print("2. Release Animal into Habitat")
     print("3. Feed Animal")
     print("4. Cultivate New Plant")
@@ -27,7 +27,7 @@ def main_menu():
     choice = input(">> ")
 
     if choice == "1":
-        annex_habitat(keahua)
+        annex_biome(keahua)
 
     if choice == "2":
         release_animal(keahua)
