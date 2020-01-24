e3b7fcc24025123ec55e45f0214e05083d42a75f
Jeremiah Bell <jeremiah.bell812.topper@gmail.com>
2020-01-23 23:24:31 -0600

    created lists for prey or food or critter vittles as lists in new folder under animals

---
+++ animals/preyCritters/critter_vittles.py
@@ -0,0 +1,15 @@
+# Tuple? List? Dictionary? or Set?
+# Why Am I using this and What does it go to next?
+# Jb - Choice: List A list is iterable (can be iterated through?) ordered, and can be altered.
+
+# lists of critters(prey animals) for vittles(sustenance for the animals we like and don't want eaten) 
+
+rodents = ["House Mouse", "Black Rat", "Polynesian Rat", "Mickey Mouse", "Ratatouille"]
+
+fish = ["Yellow Fin Tuna", "Mahi-Mahi", "Marlin", "Wahoo", "Ono", "Hapu'upu'u", "Kajiki", "Opakapaku", "Monchong"]
+
+insects = ["centipedes", "cockroaches", "Assassin Bug", "Golden Tortoise Beetle", "Dobsonflies", "The Eastern Velvet Ant (Cow-Killer)"]
+
+veggies = ["Candlenut", "Pineapple", "Breadfruit", "Edible Canna", "Seagrape", "Papaya", "Coconut"]
+
+
