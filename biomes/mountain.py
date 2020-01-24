70d110576da0ad7854d10e4837adcc0f08165c71
Charles Jackson <cpjackson572@gmail.com>
2020-01-24 15:03:09 -0600

    finished initial/base definition of mountain class

--- biomes/mountain.py
+++ biomes/mountain.py
@@ -1,7 +1,13 @@
-from interfaces.habitats import IContainsAnimals
-from interfaces.habitats import IContainsPlants
+from interfaces.habitats import IContainsAnimals, IContainsPlants
 from interfaces import Identifiable
 
 class Mountain(IContainsAnimals, IContainsPlants, Identifiable):
-    def __init__(self):
-        
+    
+    def __init__(self, name):
+        IContainsAnimals.__init__(self)
+        IContainsPlants.__init__(self)
+        Identifiable.__init__(self)
+        self.name = name
+    
+    def __str__(self):
+        return f'{self.name} Mountain.'
