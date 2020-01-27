31d910c524c4dc5fdf1de81848c8bd4f14b9123e
Jeremiah Bell <jeremiah.bell812.topper@gmail.com>
2020-01-27 13:45:15 -0600

    trying to get animals to release

--- actions/release_animal.py
+++ actions/release_animal.py
@@ -3,20 +3,52 @@ from animals import RiverDolphin
 def release_animal(arboretum):
     animal = None
 
-    print("1. River Dolphin")
-    print("2. Dragonfly")
+    print("1. Gold Dust Day Gecko")
+    print("2. River Dolphin")
+    print("3. Nene Goose")
+    print("4. KiKakapu")
+    print("5. Pueo")
+    print("6. 'Ulae")
+    print("7. Ope'ape'a")
+    print("8. Happy-Face Spider")
 
-    choice = input("Choose animal to release > ")
+    choice = input("Choose animal to release into the chosen habitat > ")
 
     if choice == "1":
+        animal = GoldGecko()
+
+    elif choice == "2":
         animal = RiverDolphin()
 
-    if choice == "2":
-        pass
+    elif choice == "3":
+        animal = NeneGoose()
+
+    elif choice == "4":
+        animal = Kikakapu()
+
+    elif choice == "5":
+        animal = Pueo()
+
+    elif choice == "6":
+        animal = Ulae()
+
+    elif choice == "7":
+        animal = Opeapea()
+
+    elif choice == "8":
+        animal = HappySpider()
+        
+    else:
+        input(" That sucked try again! ")
+        return
+    
+    if isinstance(animal, Grasslandy):
+        for eachitem in arboretum.grasslands:
+            arboretum.grasslands.append(eachitem)
 
 
-    for index, river in enumerate(arboretum.rivers):
-        print(f'{index + 1}. River {river.id}')
+    # for index, river in enumerate(arboretum.rivers):
+    #     print(f'{index + 1}. River {river.id}')
 
     print("Release the animal into which biome?")
     choice = input("> ")
