488db44a3ac41740b01c6f7c5f1c307c51beb6d7
Caroline Brownlee <brownleecaroline@gmail.com>
2020-01-27 15:37:48 -0600

    added another animal class

---
+++ animals/Kīkākapu.py
@@ -0,0 +1,24 @@
+from interfaces.animals import IsFreshwater
+from animals import Animal
+from interfaces import Identifiable
+
+class Kīkākapu(Animal, IsFreshwater, Identifiable):
+    def __init__(self):
+        Animal.__init__(self, "Nene Goose")
+        IsFreshwater.__init__(self)
+        Identifiable.__init__(self)
+        self.__prey = { "Vegetation" }
+
+    @property
+    def prey(self):
+        return self.__prey
+    
+    def feed(self, prey):
+        if prey in self.__prey:
+            print(f'The Kīkākapu ate {prey} for a meal')
+        else:
+            print(f'The Kīkākapu rejects the {prey}')
+    
+    def __str__(self):
+        return f'Kīkākapu {self.id}.'
+    
