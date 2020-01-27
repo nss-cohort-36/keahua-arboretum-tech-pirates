4b076f909f63f13d910bb8f79d400c766de391b1
Jeremiah Bell <jeremiah.bell812.topper@gmail.com>
2020-01-27 11:52:15 -0600

    saving before merge

--- biomes/mountain.py
+++ biomes/mountain.py
@@ -4,13 +4,12 @@ from interfaces import Identifiable
 
 class Mountain(IContainsAnimals, IContainsPlants, Identifiable):
     
-    def __init__(self, name):
+    def __init__(self):
         IContainsAnimals.__init__(self)
         IContainsPlants.__init__(self)
         Identifiable.__init__(self)
-        self.name = name
     
     def __str__(self):
-        return f'{self.name} Mountain.'
+        return f'Mountain.'
 
   
