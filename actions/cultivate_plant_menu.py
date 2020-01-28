f38a8d6d78c81c3682b9f148da41147a63ef21ed
Sam Pita <samanthakpita@gmail.com>
2020-01-28 13:25:45 -0600

    added choices to cultivate_plant_menu

--- actions/cultivate_plant_menu.py
+++ actions/cultivate_plant_menu.py
@@ -3,13 +3,14 @@
 import os
 import sys
 sys.path.append('../')
+from arboretum import Arboretum
 from plants import Blue_Jade_Vine
 from plants import Mountain_Apple_Tree
 from plants import RainbowTree
 from plants import Silversword
 # from index import main_menu
 
-def cultivate_plant_menu(arboretum):
+def cultivate_plant_menu(Arboretum):
     os.system('cls' if os.name == 'nt' else 'clear')
     print("1. Mountain Apple Tree")
     print("2. Silversword")
@@ -20,16 +21,16 @@ def cultivate_plant_menu(arboretum):
     choice = input("Choose plant to culitivate > ")
 
     if choice == "1":
-        pass
+        plant_to_cultivate = RainbowTree()
      
     if choice == "2":
-        pass
+        plant_to_cultivate = Silversword()
 
     if choice == "3":
-        pass
+        plant_to_cultivate = Blue_Jade_Vine()
 
     if choice == "4":
-        pass
+        plant_to_cultivate = Mountain_Apple_Tree()
 
     # if choice == "5":
     #     main_menu()
