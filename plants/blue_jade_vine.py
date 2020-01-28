f4fd2b0c0e0782ca011e1f1d2be303d224f5fcf4
Sam Pita <samanthakpita@gmail.com>
2020-01-28 11:49:15 -0600

    added plant classes

--- plants/blue_jade_vine.py
+++ plants/blue_jade_vine.py
@@ -2,6 +2,6 @@ from plants import Plant
 from interfaces.identifiable import Identifiable
 
 class Blue_Jade_Vine(Plant, Identifiable):
-    def__init__(self):
+    def __init__(self):
         Plant.__init__(self, "Blue Jade Vine", "partial", 0, "Medium")
         
