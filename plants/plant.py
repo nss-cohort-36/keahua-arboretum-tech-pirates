f7297a53100f668b53bf0445bf46912cb09967a7
Charles Jackson <cpjackson572@gmail.com>
2020-01-27 23:23:33 -0600

    added comments on plant.py

--- plants/plant.py
+++ plants/plant.py
@@ -1,5 +1,5 @@
 class Plant:
 
-    def __init__(self, species, season):
+    def __init__(self, species, season): # <-- delete season and add sunlight, seeds_produced, and insecticide_resistance?
       self.species = species
       self.peak_season = season
