e216232d661cdc4442466460740ab074280574cc
Sam Pita <samanthakpita@gmail.com>
2020-01-29 09:52:07 -0600

    commit

--- actions/biome_menu.py
+++ actions/biome_menu.py
@@ -11,52 +11,38 @@ def biome_menu(arboretum):
     # biome_types = [Arboretum.rivers, Arboretum.grasslands, Arboretum.mountains, Arboretum.forests, Arboretum.swamps, Arboretum.coastlines
     # ]
     
-    
+    # iterate through the biomes dictionary in Arboretum()
     
     for index, biome in enumerate(arboretum.biomes):
         print({index + 1}, biome)
     
     choice = input("Choose a biome type for your plant > ")
-    
+
+
+
+
+
+
+
+
     # capture user input
     # use index position
-    if choice == "1":
-        River.add_plant(plant)
+    # if choice == "1":
+    #     River.add_plant(plant)
     
-    if choice == "2":
-        Coastline.add_plant(plant)
+    # if choice == "2":
+    #     Coastline.add_plant(plant)
     
-    if choice == "3":
-        Mountain.add_plant(plant)
+    # if choice == "3":
+    #     Mountain.add_plant(plant)
     
-    if choice == "4":
-        Grassland.add_plant(plant)
+    # if choice == "4":
+    #     Grassland.add_plant(plant)
         
-    if choice == "5":
-        Swamp.add_plant(plant)
+    # if choice == "5":
+    #     Swamp.add_plant(plant)
     
-    if choice == "6":
-        Forest.add_plant(plant)
-    # for index, river in enumerate(arboretum.rivers):
-    #     print(f'{index + 1}. River {river.id}')
+    # if choice == "6":
+    #     Forest.add_plant(plant)
+    
 
-    # for index, coastline in enumerate(arboretum.coastlines):
-    #     print(f'{index + 2}. Coastline {coastline.id}')
-
-    # for index, mountain in enumerate(arboretum.mountains):
-    #     print(f'{index + 3}. Mountain {mountain.id}')
-
-    # for index, grassland in enumerate(arboretum.grasslands):
-    #     print(f'{index + 4}. Grassland {grassland.id}')
-
-    # for index, swamp in enumerate(arboretum.swamps):
-    #     print(f'{index + 5}. Swamp {swamp.id}')
-
-    # for index, forest in enumerate(arboretum.forests):
-    #     print(f'{index + 6}. Forest {forest.id}')
-
-# biome_types = [Arboretum.rivers, Arboretum.grasslands, Arboretum.mountains, Arboretum.forests, Arboretum.swamps, Arboretum.coastlines
-# ]
-
-# for index, biome_type in enumerate(biome_types):
-#     print({index}, type)
