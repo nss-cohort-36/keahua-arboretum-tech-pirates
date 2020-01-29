bfe48f2701f9cbe69195aa782d8ece05ad63c15d
Jeremiah Bell <jeremiah.bell812.topper@gmail.com>
2020-01-29 01:01:41 -0600

    why won't this work...

--- interfaces/animals/__init__.py
+++ interfaces/animals/__init__.py
@@ -2,7 +2,3 @@ from .freshwater import IsFreshwater
 from .saltwater import IsSaltwater
 from .terrestrial import IsTerrestrial
 from .aquatic import IsAquatic
-from .grasslandy import Grasslandy
-from .foresty import Foresty
-from .mountainy import Mountainy
-from interfaces.identifiable import Identifiable
---
+++ interfaces/habitats/foresty.py
@@ -0,0 +1,7 @@
+from .terrestrial import Terrestrial
+
+class Forest(Terrestrial):
+
+    def __init__(self):
+        super().__init__()
+        self.foresty = True
